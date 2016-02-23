26965b91c1a830cca7542069764e4ff55d4e544b
Nicholas Farley <ohcrap524@gmail.com>
2016-02-23 13:09:47 -0500

    Add middle.php

---
+++ php/middle.php
@@ -0,0 +1,14 @@
+<?php 
+$json = json_encode(array(
+  "user" => $_GET["ucid"],
+  "pass" => $_GET["password"]
+));
+
+$cr = curl_init();
+curl_setopt($cr, CURLOPT_URL, "http://osl84.njit.edu/~ejp9/CS490-middleend/auth.php");
+curl_setopt($cr, CURLOPT_POST, true);
+curl_setopt($cr, CURLOPT_POSTFIELDS, $json);
+curl_setopt($cr, CURLOPT_RETURNTRANSFER, true);
+
+$request = curl_exec($cr);
+?>
