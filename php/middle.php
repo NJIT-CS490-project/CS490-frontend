69dd1fde288ba9360e2d8ea41efed894d2b1da0c
Nicholas Farley <ohcrap524@gmail.com>
2016-02-23 13:28:39 -0500

    Add error logging and json header to middle.php

--- php/middle.php
+++ php/middle.php
@@ -9,8 +9,12 @@ curl_setopt($cr, CURLOPT_URL, "http://osl84.njit.edu/~ejp9/CS490-middleend/auth.
 curl_setopt($cr, CURLOPT_POST, true);
 curl_setopt($cr, CURLOPT_POSTFIELDS, $json);
 curl_setopt($cr, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($cr, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
 
 $request = curl_exec($cr);
+
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+error_log($status);
 
 echo $request;
 ?>
