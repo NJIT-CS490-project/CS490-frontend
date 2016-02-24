3458886acea2b8c1abbf546c998169f6b5ae3cb2
Nicholas Farley <ohcrap524@gmail.com>
2016-02-24 18:02:21 -0500

    Add addClass method to element library

--- js/lib/element.js
+++ js/lib/element.js
@@ -7,4 +7,9 @@
     if (element.classList) element.classList.remove(className);
     else element.className = element.className.replace(className, '');
   };
+
+  root.addClass = (element, className) => {
+    if (element.classList) element.classList.add(className);
+    else element.className += ` ${className}`;
+  };
 })();
