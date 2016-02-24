1c063fcb61090b5471ef4e590acab3ad27de2837
Nicholas Farley <ohcrap524@gmail.com>
2016-02-23 19:48:21 -0500

    Add empty form highlighting to listener

--- js/app.js
+++ js/app.js
@@ -9,6 +9,17 @@
         return window.lib.ajax.get('php/middle.php', params);
       })
       .then(response => console.log(response))
-      .catch(err => console.dir(err));
+      .catch(err => {
+        if (err.name !== 'FormValidationError') return err;
+        Array.from(form.children)
+          .map(element => {
+            element.className = element.className.replace('empty-input', '');
+            return element;
+          })
+          .filter(inputElement => !inputElement.textLength)
+          .forEach(element => {
+            element.className += ' empty-input';
+          });
+      });
   });
 })();
