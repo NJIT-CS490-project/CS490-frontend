f8bcd801925d2caf70873e872de2961c35b9e844
Nicholas Farley <ohcrap524@gmail.com>
2016-03-22 18:46:21 -0400

    Add debounce function to custom functional library

--- js/lib/f.js
+++ js/lib/f.js
@@ -4,4 +4,13 @@
   const exports = window.lib.f;
 
   exports.partial = (func, ...args) => func.bind(this, ...args);
+
+  exports.debounce = (func, wait) => {
+    let timeout;
+    return (...args) => {
+      const later = exports.partial(func, ...args);
+      window.clearTimeout(timeout);
+      timeout = window.setTimeout(later, wait);
+    };
+  };
 }
