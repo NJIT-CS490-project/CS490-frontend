8dcc26be6fc4312e5d0ffd783b16eae654748a21
Nicholas Farley <ohcrap524@gmail.com>
2016-03-23 22:38:19 -0400

    Fix search buffer "remembering" empty input

--- js/dashboard.js
+++ js/dashboard.js
@@ -7,7 +7,6 @@
 
   const searchInput = stream.fromEvent(document.getElementById('search'), 'input');
   const searchContents = stream.map(searchInput, x => x.target.value);
-  const longSearchContents = stream.filter(searchContents, x => x.length > 2);
-  const debouncedLongSearchContents = stream.debounce(longSearchContents, 500);
-  stream.log(debouncedLongSearchContents, 'contents');
+  const debouncedSearch = stream.debounce(searchContents, 500);
+  const minimumLengthSearch = stream.filter(debouncedSearch, x => x.length > 2);
 }
