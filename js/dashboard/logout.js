04e4993cb500017188a4f5775c8a5064ab8472a6
Nicholas Farley <ohcrap524@gmail.com>
2016-03-26 23:20:44 -0400

    Extract logout logic to seperate file

---
+++ js/dashboard/logout.js
@@ -0,0 +1,7 @@
+{
+  const stream = window.lib.stream;
+
+  const logoutButton = document.querySelector('[value="Logout"]');
+  const logoutStream = stream.fromEvent(logoutButton, 'click');
+  stream.log(logoutStream, 'Logout Click Stream');
+}
