523c5171ec5b1c64555d9a3fac9a9ff83d1eef81
Nicholas Farley <ohcrap524@gmail.com>
2016-03-27 13:55:09 -0400

    Add view function for events

---
+++ js/views/event.js
@@ -0,0 +1,22 @@
+{
+  window.views = window.views || {};
+  const exports = window.views;
+  exports.eventView = model => {
+    const name = model.name || 'NAME';
+    const timeStart = model.timeStart || 'TIME_START';
+    const timeEnd = model.timeEnd || 'TIME_END';
+    const location = model.location || 'LOCATION';
+    return `
+      <article class="event">
+        <img src="images/event.png"></img>
+        <section>
+          <h1>${name}</h1>
+          <p>1970-01-01}</p>
+          <p>${timeStart} to ${timeEnd}</p>
+          <p>${location}</p>
+          <input type="button" value="Delete" class="button secondary-color warning-bg-color"></input>
+        </section>
+      </article>
+    `;
+  };
+}
