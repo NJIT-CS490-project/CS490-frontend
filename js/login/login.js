d8ce2616502850f10a3ef18fcc5d947c4bfb0c2e
Nicholas Farley <ohcrap524@gmail.com>
2016-03-28 21:22:33 -0400

    Implement login button enabling based on fields

---
+++ js/login/login.js
@@ -0,0 +1,21 @@
+{
+  const stream = window.lib.stream;
+
+  const allFilledStream = inputElements => {
+    return inputElements
+      .map(inputElement => stream.fromEvent(inputElement, 'input'))
+      .map(inputStream => stream.map(inputStream, event => event.target.value))
+      .map(textStream => stream.map(textStream, text => text.length > 0))
+      .reduce(stream.and);
+  };
+
+  const loginForm = document.getElementById('login');
+  const usernameInput = loginForm.querySelector('[type="text"]');
+  const passwordInput = loginForm.querySelector('[type="password"]');
+  const loginButton = loginForm.querySelector('[type="button"]');
+
+  const loginFilled = allFilledStream([usernameInput, passwordInput]);
+  stream.subscribe(loginFilled, isFilled => {
+    loginButton.disabled = !isFilled;
+  });
+}
