28580eee443ff86070bc08adb74777b4a2eec6ec
Nicholas Farley <ohcrap524@gmail.com>
2016-03-30 19:45:10 -0400

    Change middle.php to be a transparent proxy to middle

--- php/middle.php
+++ php/middle.php
@@ -1,17 +1,53 @@
-<?php 
-$json = json_encode(array(
-  "user" => $_GET["ucid"],
-  "pass" => $_GET["password"]
-));
+<?php
 
-$cr = curl_init();
-curl_setopt($cr, CURLOPT_URL, "http://osl84.njit.edu/~ejp9/CS490-middleend/auth.php");
-curl_setopt($cr, CURLOPT_POST, true);
-curl_setopt($cr, CURLOPT_POSTFIELDS, $json);
-curl_setopt($cr, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($cr, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
+function remove_param($url, $key) {
+  $url = preg_replace('/(.*)(?|&)' . $key . '=[^&]+?(&)(.*)/i', '$1$2$4', $url . '&');
+  $url = substr($url, 0, -1);
+  return $url;
+}
 
-$request = curl_exec($cr);
 
-echo $request;
-?>
+$middle = 'http://scooterlabs.com/echo';
+$request = $_SERVER['REQUEST_URI'];
+$endpoint = $_GET['endpoint'];
+$url = remove_param($middle . $endpoint . $request, 'endpoint');
+$ch = curl_init();
+$headers = getallheaders();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  curl_setopt($ch, CURLOPT_POST, TRUE);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+}
+
+if (isset($headers['Cookie'])) {
+  curl_setopt($ch, CURLOPT_COOKIE, $headers['Cookie']);
+}
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_HEADER, TRUE);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+$response = curl_exec($ch);
+
+
+$header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+$headers = substr($response, 0, $header_size);
+$body = substr($response, $header_size);
+$headerArray = explode(PHP_EOL, $headers);
+
+foreach($headerArray as $header) {
+  $colonPos = strpos($header, ':');
+
+  if ($colonPos !== FALSE) {
+    $headerName = substr($header, 0, $colonPos);
+    if (trim($headerName) == 'Content-Encoding') continue;
+    if (trim($headerName) == 'Content-Length') continue;
+    if (trim($headerName) == 'Transfer-Encoding') continue;
+  }
+
+  header($header, FALSE);
+}
+
+echo $body;
+
+curl_close($ch);
