a674de732bf0e5709cf514b2f154fbc910abe6f9
Nicholas Farley <ohcrap524@gmail.com>
2016-03-30 21:53:10 -0400

    Implement final registration functionality

--- js/login/register.js
+++ js/login/register.js
@@ -1,5 +1,6 @@
 {
   const stream = window.lib.stream;
+  const time = window.lib.time;
 
   const fieldsMatch = fields => {
     return fields
@@ -18,7 +19,6 @@
 
   const registerForm = document.getElementById('register');
   const registerButton = registerForm.querySelector('[type="button"]');
-
   const usernameInput = registerForm.querySelector('[type="text"]');
   const passwordFields = registerForm.querySelectorAll('[type="password"]');
   const passwordInput = passwordFields[0];
@@ -27,16 +27,27 @@
   const passwordsMatch = fieldsMatch([passwordInput, confirmPasswordInput]);
   const allFilled = fieldsFilled([passwordInput, confirmPasswordInput, usernameInput]);
   const showButton = stream.and(passwordsMatch, allFilled);
-
-  stream.log(passwordsMatch, 'Passwords Match');
-  stream.log(allFilled, 'All Fields Filled');
-
   stream.subscribe(showButton, shouldEnable => {
     registerButton.disabled = !shouldEnable;
   });
 
+  const usernameProperty = stream.map(stream.fromEvent(usernameInput, 'input'), event => event.target.value);
+  const passwordProperty = stream.map(stream.fromEvent(passwordInput, 'input'), event => event.target.value);
   const registerClick = stream.fromEvent(registerButton, 'click');
-  stream.subscribe(stream.debounce(registerClick, 1000), () => {
-    alert('Account successfully created!');
+  stream.subscribe(stream.debounce(registerClick, 250), () => {
+    const requestOptions = {
+      method: 'POST',
+      body: JSON.stringify({
+        username: stream.value(usernameProperty),
+        password: stream.value(passwordProperty),
+      }),
+    };
+
+    const request = fetch('php/middle.php?endpoint=create.php', requestOptions);
+    Promise.race([request, time.timeout(5000, 'Registration timed out')])
+      .then(response => (response.statusText === 'OK') ? response.json() : Promise.reject(response.statusText))
+      .then(json => (json.message === 'Successfully created user') ? Promise.resolve() : Promise.reject(json.message))
+      .then(() => alert('Account successfully created!'))
+      .catch(error => alert(error));
   });
 }
