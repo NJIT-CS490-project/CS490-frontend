8ce4839afd14b8519fee67c7aad3101128691196
Nicholas Farley <ohcrap524@gmail.com>
2016-03-31 18:48:05 -0400

    Update eventView function with delete button logic

--- js/views/event.js
+++ js/views/event.js
@@ -1,20 +1,30 @@
 {
   window.views = window.views || {};
   const exports = window.views;
-  exports.eventView = model => {
-    const name = model.name || 'NAME';
-    const timeStart = model.timeStart || 'TIME_START';
-    const timeEnd = model.timeEnd || 'TIME_END';
-    const location = model.location || 'LOCATION';
+
+  exports.eventView = (model, requester, isAdmin) => {
+    const title = model.title || '';
+    const date = model.date || '';
+    const startTime = model.startTime || '';
+    const endTime = model.endTime || '';
+    const location = model.location || '';
+    const id = model.id;
+    const creatorName = model.username;
+
+    const deleteButtonHTML = ((requester === creatorName) || isAdmin)
+                             ? `<input data-id="${id}" type="button" value="Delete" class="button secondary-color warning-bg-color"></input>`
+                             : '';
+
     return `
       <article class="event">
         <img src="images/event.png"></img>
         <section>
-          <h1>${name}</h1>
-          <p>1970-01-01</p>
-          <p>${timeStart} to ${timeEnd}</p>
+          <h1>${title}</h1>
+          <p>${date}</p>
+          <p>${startTime} to ${endTime}</p>
           <p>${location}</p>
-          <input type="button" value="Delete" class="button secondary-color warning-bg-color"></input>
+          <p>hosted by ${creatorName}<p>
+          ${deleteButtonHTML}
         </section>
       </article>
     `;
