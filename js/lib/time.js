c3b67f3adc39ad88212d94b1e75080e9589f3576
Nicholas Farley <ohcrap524@gmail.com>
2016-03-30 20:37:37 -0400

    Add timeout promise to custom time library

--- js/lib/time.js
+++ js/lib/time.js
@@ -6,4 +6,11 @@
   exports.delay = (milliseconds) => (argument) => new Promise(fulfill => {
     setInterval(fulfill.bind(undefined, argument), milliseconds);
   });
+
+  exports.timeout = (milliseconds, errMessage) => new Promise((_, reject) => {
+    errMessage = errMessage || `Timed out in ${milliseconds} milliseconds`;
+    setTimeout(() => {
+      reject(new Error(errMessage));
+    }, milliseconds);
+  });
 }
