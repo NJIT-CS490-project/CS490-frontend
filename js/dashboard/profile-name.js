adae0e4d6504afea93c6b8c126c11d8d5189f5de
Nicholas Farley <ohcrap524@gmail.com>
2016-03-31 22:53:17 -0400

    Fix issue with profile-name being undefined

--- js/dashboard/profile-name.js
+++ js/dashboard/profile-name.js
@@ -1,11 +1,11 @@
 {
   const profileName = document.getElementById('profile-name');
 
-  document.addEventListener('DOMContentLoaded', event => {
+  document.addEventListener('DOMContentLoaded', () => {
     fetch('php/middle.php?endpoint=self.php')
       .then(response => response.json())
       .then(json => {
-        profileName.innerHTML = json.username;
+        profileName.innerHTML = json.username || '';
       })
       .catch(err => console.error(err));
   });
