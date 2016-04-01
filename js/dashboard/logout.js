b5a4fb8cad8a34cd352c27b7aa5d6dc7037d1343
Nicholas Farley <ohcrap524@gmail.com>
2016-04-01 14:29:24 -0400

    Delete session cookie on logout

--- js/dashboard/logout.js
+++ js/dashboard/logout.js
@@ -14,6 +14,9 @@
     const request = fetch('php/middle.php?endpoint=logout.php', requestOptions);
     Promise.race([request, time.timeout(5000, 'Logout timed out. (?)')])
       .then(response => (response.statusText === 'OK') ? Promise.resolve() : Promise.reject(response.statusText))
+      .then(() => {
+        document.cookie = 'PHPSESSID=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+      })
       .then(() => { window.location = 'index.html'; })
       .catch(error => console.error(error));
   });
