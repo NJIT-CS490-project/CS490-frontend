75882c9798ad85b620c8599981215c20fb84f216
Nicholas Farley <ohcrap524@gmail.com>
2016-04-01 13:42:49 -0400

    Allow passing cookies on other self call

--- js/dashboard/profile-name.js
+++ js/dashboard/profile-name.js
@@ -2,7 +2,7 @@
   const profileName = document.getElementById('profile-name');
 
   document.addEventListener('DOMContentLoaded', () => {
-    fetch('php/middle.php?endpoint=self.php')
+    fetch('php/middle.php?endpoint=self.php', { credentials: 'same-origin' })
       .then(response => response.json())
       .then(json => {
         profileName.innerHTML = json.username || '';
