8e7328ae2ad53b441838090388bd86cb3f603f80
Nicholas Farley <ohcrap524@gmail.com>
2016-04-08 23:22:46 -0400

    Refactor logout to use new stream api

--- js/dashboard/logout.js
+++ js/dashboard/logout.js
@@ -1,23 +1,30 @@
 {
-  const stream = window.lib.stream;
+  const Stream = window.lib.Stream;
   const time = window.lib.time;
 
   const logoutButton = document.querySelector('[value="Logout"]');
-  const logoutStream = stream.fromEvent(logoutButton, 'click');
+  Stream
+    .fromEvent(logoutButton, 'click')
+    .subscribe(() => {
+      const requestOptions = {
+        method: 'PUT',
+        credentials: 'same-origin',
+      };
+      const parallels = [
+        fetch('php/middle.php?endpoint=logout.php', requestOptions),
+        time.timeout(5000, 'Logout timed out. (?)'),
+      ];
 
-  stream.subscribe(logoutStream, () => {
-    const requestOptions = {
-      method: 'PUT',
-      credentials: 'same-origin',
-    };
-
-    const request = fetch('php/middle.php?endpoint=logout.php', requestOptions);
-    Promise.race([request, time.timeout(5000, 'Logout timed out. (?)')])
-      .then(response => (response.statusText === 'OK') ? Promise.resolve() : Promise.reject(response.statusText))
-      .then(() => {
-        document.cookie = 'PHPSESSID=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-      })
-      .then(() => { window.location = 'index.html'; })
-      .catch(error => console.error(error));
-  });
+      Promise.race(parallels)
+        .then(response => (response.statusText === 'OK')
+          ? Promise.resolve()
+          : Promise.reject(response.statusText))
+        .then(() => {
+          document.cookie = 'PHPSESSID=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        })
+        .then(() => {
+          window.location = 'index.html';
+        })
+        .catch(error => console.error(error));
+    });
 }
